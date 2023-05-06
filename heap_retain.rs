97371dadbcc7c84928b267294e26af24b74774e7
Dan Nguyen <pedantic@gmail.com>
2023-05-06 14:03:00 -0500

    common: Fix miri warning

--- heap_retain.rs
+++ heap_retain.rs
@@ -1,6 +1,7 @@
 /// Max-Heap
 pub fn accumulate_max_n<const N: usize, T: Ord>(mut heap: [T; N], x: T) -> [T; N] {
     for i in 0..N {
+        assert!(i < heap.len());
         if x > heap[i] {
             // SAFETY: `i` will always be within the array bounds.
             // We will always make an empty slot. So at most we will need to
@@ -9,10 +10,11 @@ pub fn accumulate_max_n<const N: usize, T: Ord>(mut heap: [T; N], x: T) -> [T; N
             // are copied (N - 1 - (N - 1) == 0).
             // see: https://doc.rust-lang.org/1.65.0/src/alloc/vec/mod.rs.html#1392-1406
             unsafe {
-                let p = heap.as_mut_ptr().add(i);
-
                 // Drop the last element
                 std::ptr::drop_in_place(heap.as_mut_ptr().add(N - 1));
+
+                // Get ptr to first element
+                let p = heap.as_mut_ptr().add(i);
 
                 // Copy one less than the elements on the right, on spot over
                 std::ptr::copy(p, p.add(1), N - 1 - i);
