9055bfe4fbf0758c303e9e4a62e7e314eb83c555
Dan Nguyen <pedantic@gmail.com>
2024-12-09 20:26:52 -0600

    Day09: Replace tuple with struct

--- src/day09.rs
+++ src/day09.rs
@@ -2,37 +2,64 @@ use std::iter;
 
 use aoc_runner_derive::{aoc, aoc_generator};
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Object {}
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct Group {
+    id: i64,
+    len: usize,
+}
+
+impl std::fmt::Display for Group {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for _ in 0..self.len {
+            if self.id != -1 {
+                write!(f, "{}", self.id)?;
+            } else {
+                write!(f, ".")?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+impl Group {
+    fn new(id: i64, count: usize) -> Self {
+        Self { id, len: count }
+    }
+}
 
 #[aoc_generator(day9)]
-pub fn generator(input: &str) -> Vec<i64> {
-    let mut output = Vec::new();
+pub fn generator(input: &str) -> (Vec<i64>, Vec<Group>) {
+    let mut p1 = Vec::new();
+    let mut p2 = Vec::new();
     let mut file_id = 0;
     for (c, is_file) in input.chars().zip([true, false].into_iter().cycle()) {
         let n = c.to_digit(10).unwrap() as usize;
         if is_file {
-            output.extend(iter::repeat(file_id).take(n));
+            p1.extend(iter::repeat(file_id).take(n));
+            p2.push(Group::new(file_id, n));
             file_id += 1;
         } else {
-            output.extend(iter::repeat(-1).take(n));
+            p1.extend(iter::repeat(-1).take(n));
+            p2.push(Group::new(-1, n));
         }
     }
 
-    output
+    (p1, p2)
 }
 
 #[aoc(day9, part1)]
-pub fn part1(inputs: &[i64]) -> usize {
-    let mut inputs = inputs.to_vec();
+pub fn part1(inputs: &(Vec<i64>, Vec<Group>)) -> usize {
+    let mut inputs = inputs.0.to_vec();
     let mut len = inputs.len();
-    let mut i = 0;
 
+    let mut i = 0;
     while i < len {
         if inputs[i] == -1 {
             inputs.swap(i, len - 1);
             inputs.pop();
 
+            // remove some at the end
             while Some(-1) == inputs.last().copied() {
                 inputs.pop();
             }
@@ -41,98 +68,61 @@ pub fn part1(inputs: &[i64]) -> usize {
         i += 1;
     }
 
-    // println!(">> {:?}", inputs);
-
     inputs
         .iter()
         .enumerate()
-        .map(|(i, c)| (*c as usize) * i)
+        .map(|(idx, &c)| (c as usize) * idx)
         .sum()
 }
 
-fn variable_window(inputs: &[i64]) -> Vec<(i64, usize)> {
-    let mut prev = None;
-    let mut count = 0;
-    let mut res = Vec::new();
-
-    for &x in inputs.iter() {
-        match prev {
-            None => {
-                prev = Some(x);
-                count = 1;
-            }
-            Some(y) if x == y => {
-                count += 1;
-            }
-            Some(y) => {
-                res.push((y, count));
-                prev = Some(x);
-                count = 1;
-            }
-        }
+pub fn print_window(inputs: &[Group]) {
+    for g in inputs {
+        print!("{}", g);
     }
-
-    if count > 0 {
-        if let Some(prev) = prev {
-            res.push((prev, count));
-        }
-    }
-
-    res
-}
-
-pub fn print_window(inputs: &[(i64, usize)]) {
-    for &(i, count) in inputs {
-        for _ in 0..count {
-            if i != -1 {
-                print!("{}", i);
-            } else {
-                print!(".");
-            }
-        }
-    }
+    println!();
 }
 
 #[aoc(day9, part2)]
-pub fn part2(inputs: &[i64]) -> usize {
-    let mut inputs = variable_window(inputs);
-    let len = inputs.len();
-    let mut i = 0;
+pub fn part2(inputs: &(Vec<i64>, Vec<Group>)) -> usize {
+    let mut inputs = inputs.1.to_vec();
+    let mut len = inputs.len();
 
-    while i < len {
-        if inputs[i].0 == -1 {
-            for pos in ((i + 1)..len).rev() {
-                if inputs[pos].0 != -1 && inputs[pos].1 <= inputs[i].1 {
-                    inputs.swap(i, pos);
+    let mut idx = 0;
+    while idx < len {
+        if inputs[idx].id == -1 {
+            for pos in ((idx + 1)..len).rev() {
+                if inputs[pos].id != -1 && inputs[pos].len <= inputs[idx].len {
+                    inputs.swap(idx, pos);
 
-                    if inputs[pos].1 != inputs[i].1 {
+                    if inputs[pos].len != inputs[idx].len {
                         // fix length
-                        let new_len = inputs[pos].1 - inputs[i].1;
-                        inputs[pos].1 = inputs[i].1;
-                        inputs.insert(i + 1, (-1, new_len));
+                        let new_len = inputs[pos].len - inputs[idx].len;
+                        inputs[pos].len = inputs[idx].len;
+                        inputs.insert(idx + 1, Group::new(-1, new_len));
+                        len += 1;
                     }
+
                     // print_window(&inputs);
-                    // println!("");
                     // println!("swapping, {} and {}: ", i, pos);
                     break;
                 }
             }
         }
-        i += 1;
+        idx += 1;
     }
 
     let mut sum = 0;
     let mut pos = 0;
-    for (i, count) in inputs {
-        for _ in 0..count {
-            if i != -1 {
-                sum += i * pos
-            }
-            pos += 1;
+
+    for Group { id, len } in inputs {
+        if id != -1 {
+            let total_pos_sum = len * (2 * pos + len - 1) / 2;
+            sum += total_pos_sum * (id as usize);
         }
+        pos += len;
     }
 
-    sum as usize
+    sum
 }
 
 #[cfg(test)]
