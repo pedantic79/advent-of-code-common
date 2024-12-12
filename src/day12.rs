646549735c4dbdbb62379f34392648910311cdc8
Dan Nguyen <pedantic@gmail.com>
2024-12-12 09:31:34 -0600

    Day12: char to u8, common solve

--- src/day12.rs
+++ src/day12.rs
@@ -10,11 +10,11 @@ use pathfinding::{
 use crate::common::utils::build_array;
 
 #[aoc_generator(day12)]
-pub fn generator(input: &str) -> Vec<Vec<char>> {
-    input.lines().map(|l| l.chars().collect()).collect()
+pub fn generator(input: &str) -> Vec<Vec<u8>> {
+    input.lines().map(|l| l.bytes().collect()).collect()
 }
 
-fn check_perimeter(inputs: &[Vec<char>], r: usize, c: usize, cell: char) -> usize {
+fn check_perimeter(inputs: &[Vec<u8>], r: usize, c: usize, cell: u8) -> usize {
     let row_max = inputs.len();
     let col_max = inputs[0].len();
 
@@ -22,8 +22,7 @@ fn check_perimeter(inputs: &[Vec<char>], r: usize, c: usize, cell: char) -> usiz
         .iter()
         .map(|&delta| {
             move_in_direction((r, c), delta, (row_max, col_max))
-                .and_then(|(r, c)| inputs.get(r).and_then(|row| row.get(c)))
-                .copied()
+                .and_then(|(r, c)| get(inputs, r, c))
         })
         .filter(|&x| x != Some(cell))
         .count()
@@ -36,17 +35,20 @@ const CORNERS: [[(isize, isize); 3]; 4] = [
     [directions::N, directions::E, directions::NE],
 ];
 
-fn check_corner(inputs: &[Vec<char>], r: usize, c: usize, cell: char) -> usize {
+fn get(inputs: &[Vec<u8>], r: usize, c: usize) -> Option<u8> {
+    inputs.get(r).and_then(|row| row.get(c)).copied()
+}
+
+fn check_corner(inputs: &[Vec<u8>], r: usize, c: usize, cell: u8) -> usize {
     let row_max = inputs.len();
     let col_max = inputs[0].len();
     let cell = Some(cell);
     let mut count = 0;
 
     for corner in CORNERS.iter() {
-        let neighbors = build_array::<Option<char>, _, 3>(corner.iter().map(|&delta| {
+        let neighbors = build_array::<Option<u8>, _, 3>(corner.iter().map(|&delta| {
             move_in_direction((r, c), delta, (row_max, col_max))
-                .and_then(|(r, c)| inputs.get(r).and_then(|row| row.get(c)))
-                .copied()
+                .and_then(|(r, c)| get(inputs, r, c))
         }));
 
         if neighbors[0] != cell && neighbors[1] != cell {
@@ -61,25 +63,23 @@ fn check_corner(inputs: &[Vec<char>], r: usize, c: usize, cell: char) -> usize {
     count
 }
 
-#[aoc(day12, part1)]
-pub fn part1(inputs: &[Vec<char>]) -> usize {
+fn solve<I>(inputs: &[Vec<u8>], check: I) -> usize
+where
+    I: Fn(&[Vec<u8>], usize, usize, u8) -> usize,
+{
     let mut seen = HashSet::default();
     let r_max = inputs.len();
     let c_max = inputs[0].len();
     let mut count = 0;
 
-    // eprintln!("{inputs:?}");
-
     for (r, row) in inputs.iter().enumerate() {
         for (c, &cell) in row.iter().enumerate() {
-            // eprintln!("{cell}");
-
             if seen.contains(&(r, c)) {
                 continue;
             }
             seen.insert((r, c));
 
-            let (mut area, mut perim) = (1, check_perimeter(inputs, r, c, cell));
+            let (mut area, mut perim) = (1, check(inputs, r, c, cell));
             let mut queue: VecDeque<(usize, usize)> = DIRECTIONS_4
                 .iter()
                 .filter_map(|&delta| move_in_direction((r, c), delta, (r_max, c_max)))
@@ -90,11 +90,10 @@ pub fn part1(inputs: &[Vec<char>]) -> usize {
                 if seen.contains(&(y, x)) {
                     continue;
                 }
-                // println!("{y},{x}={:?}", inputs.get(y).and_then(|row| row.get(x)));
 
                 seen.insert((y, x));
                 area += 1;
-                perim += check_perimeter(inputs, y, x, cell);
+                perim += check(inputs, y, x, cell);
                 queue.extend(
                     DIRECTIONS_4
                         .iter()
@@ -104,7 +103,6 @@ pub fn part1(inputs: &[Vec<char>]) -> usize {
                         }),
                 )
             }
-            // eprintln!("{cell}: {area}*{perim}={}", area * perim);
             count += area * perim;
         }
     }
@@ -112,49 +110,14 @@ pub fn part1(inputs: &[Vec<char>]) -> usize {
     count
 }
 
+#[aoc(day12, part1)]
+pub fn part1(inputs: &[Vec<u8>]) -> usize {
+    solve(inputs, check_perimeter)
+}
+
 #[aoc(day12, part2)]
-pub fn part2(inputs: &[Vec<char>]) -> usize {
-    let mut seen = HashSet::default();
-    let r_max = inputs.len();
-    let c_max = inputs[0].len();
-    let mut count = 0;
-
-    for (r, row) in inputs.iter().enumerate() {
-        for (c, &cell) in row.iter().enumerate() {
-            if seen.contains(&(r, c)) {
-                continue;
-            }
-            seen.insert((r, c));
-
-            let (mut area, mut perim) = (1, check_corner(inputs, r, c, cell));
-            let mut queue: VecDeque<(usize, usize)> = DIRECTIONS_4
-                .iter()
-                .filter_map(|&delta| move_in_direction((r, c), delta, (r_max, c_max)))
-                .filter(|delta| inputs.get(delta.0).and_then(|row| row.get(delta.1)) == Some(&cell))
-                .collect();
-
-            while let Some((y, x)) = queue.pop_front() {
-                if seen.contains(&(y, x)) {
-                    continue;
-                }
-
-                seen.insert((y, x));
-                area += 1;
-                perim += dbg!(check_corner(inputs, y, x, cell));
-                queue.extend(
-                    DIRECTIONS_4
-                        .iter()
-                        .filter_map(|&delta| move_in_direction((y, x), delta, (r_max, c_max)))
-                        .filter(|delta| {
-                            inputs.get(delta.0).and_then(|row| row.get(delta.1)) == Some(&cell)
-                        }),
-                )
-            }
-            count += area * perim;
-        }
-    }
-
-    count
+pub fn part2(inputs: &[Vec<u8>]) -> usize {
+    solve(inputs, check_corner)
 }
 
 #[cfg(test)]
