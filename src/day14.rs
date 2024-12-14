940e19b020a429106b6b2b20504ac1ffcd6ca7d9
Dan Nguyen <pedantic@gmail.com>
2024-12-14 12:05:57 -0600

    Day14: Add const generics to test sample input

--- src/day14.rs
+++ src/day14.rs
@@ -52,78 +52,85 @@ pub fn generator(input: &str) -> Vec<Robot> {
     process_input(nom_lines(parse_robot))(input)
 }
 
-#[aoc(day14, part1)]
-pub fn part1(inputs: &[Robot]) -> usize {
-    let mut robots = inputs.to_vec();
+#[allow(dead_code)]
+fn print_robots<const H: usize, const W: usize>(robots: &[Robot]) {
+    let mut grid = [[b'.'; W]; H];
 
-    robots
-        .iter_mut()
-        .for_each(|r| r.simulate(100, (HEIGHT, WIDTH)));
+    for r in robots.iter() {
+        grid[r.pos.0][r.pos.1] = b'#';
+    }
 
-    const WIDTH_MID: usize = WIDTH / 2;
-    const HEIGHT_MID: usize = HEIGHT / 2;
-    const WIDTH_MID1: usize = WIDTH_MID + 1;
-    const HEIGHT_MID1: usize = HEIGHT_MID + 1;
+    for row in grid.iter() {
+        println!("{}", unsafe { std::str::from_utf8_unchecked(&row[..]) });
+    }
+}
+
+fn solve_part1<const H: usize, const W: usize>(mut robots: Vec<Robot>) -> usize {
+    robots.iter_mut().for_each(|r| r.simulate(100, (H, W)));
+
+    let mid = (H / 2, W / 2);
 
     let mut counts = [0; 4];
     for r in robots.iter() {
-        #[allow(non_contiguous_range_endpoints)]
-        match r.pos {
-            (0..HEIGHT_MID, 0..WIDTH_MID) => counts[0] += 1,
-            (HEIGHT_MID1..HEIGHT, 0..WIDTH_MID) => counts[1] += 1,
-            (HEIGHT_MID1..HEIGHT, WIDTH_MID1..WIDTH) => counts[2] += 1,
-            (0..HEIGHT, WIDTH_MID1..WIDTH) => counts[3] += 1,
-            _ => {}
+        if r.pos.0 == mid.0 || r.pos.1 == mid.1 {
+            continue;
         }
-    }
+        let y: usize = (r.pos.0 < mid.0).into();
+        let x: usize = (r.pos.1 < mid.1).into();
 
+        counts[y << 1 | x] += 1;
+    }
     counts.iter().product()
 }
 
-#[aoc(day14, part2)]
-pub fn part2(inputs: &[Robot]) -> usize {
-    const W: isize = WIDTH as isize;
-    const H: isize = HEIGHT as isize;
+#[aoc(day14, part1)]
+pub fn part1(robots: &[Robot]) -> usize {
+    solve_part1::<HEIGHT, WIDTH>(robots.to_vec())
+}
 
-    let mut robots = inputs.to_vec();
+fn solve_part2<const H: usize, const W: usize>(mut robots: Vec<Robot>) -> usize {
     let mut fpos = Vec::with_capacity(robots.len());
 
     // Simulate the robot coordinates independently from 1 to the max of the coordinate
     // storing the coordinates as f64 in fpos
     // then calculate the statistical variance of all the positions
     // find the number of steps for the minimal variance
-    let min_xstep = (1..=W)
+    let min_xstep = (0..W)
         .map(|i| {
             fpos.clear();
             robots.iter_mut().for_each(|r| {
-                r.pos.1 = Robot::simulate_coord(r.pos.1, r.vol.1, 1, WIDTH);
+                r.pos.1 = Robot::simulate_coord(r.pos.1, r.vol.1, 1, W);
                 fpos.push(r.pos.1 as f64);
             });
 
-            (i, statistical::variance(&fpos, None))
+            (i + 1, statistical::variance(&fpos, None))
         })
         .reduce(|min, x| if x.1 < min.1 { x } else { min })
         .unwrap()
         .0;
 
     // repeat for other coordinate
-    let min_ystep = (1..=H)
+    let min_ystep = (0..H)
         .map(|i| {
             fpos.clear();
             robots.iter_mut().for_each(|r| {
-                r.pos.0 = Robot::simulate_coord(r.pos.0, r.vol.0, 1, HEIGHT);
+                r.pos.0 = Robot::simulate_coord(r.pos.0, r.vol.0, 1, H);
                 fpos.push(r.pos.0 as f64);
             });
 
-            (i, statistical::variance(&fpos, None))
+            (i + 1, statistical::variance(&fpos, None))
         })
         .reduce(|min, x| if x.1 < min.1 { x } else { min })
         .unwrap()
         .0;
 
     // chinese remainder theorem
-    let ans = chinese_remainder_theorem([(min_xstep, W), (min_ystep, H)].into_iter());
-    ans.try_into().unwrap()
+    chinese_remainder_theorem([(min_xstep, W), (min_ystep, H)].into_iter())
+}
+
+#[aoc(day14, part2)]
+pub fn part2(robots: &[Robot]) -> usize {
+    solve_part2::<HEIGHT, WIDTH>(robots.to_vec())
 }
 
 #[cfg(test)]
@@ -143,6 +150,9 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3";
 
+    const S_HEIGHT: usize = 7;
+    const S_WIDTH: usize = 11;
+
     #[test]
     pub fn input_test() {
         println!("{:?}", generator(SAMPLE));
@@ -152,11 +162,21 @@ p=9,5 v=-3,-3";
 
     #[test]
     pub fn part1_test() {
-        // assert_eq!(part1(&generator(SAMPLE)), 12);
+        assert_eq!(solve_part1::<S_HEIGHT, S_WIDTH>(generator(SAMPLE)), 12);
     }
 
     #[test]
-    pub fn part2_test() {}
+    pub fn part2_test() {
+        let mut robots = generator(SAMPLE);
+        let steps = solve_part2::<S_HEIGHT, S_WIDTH>(robots.clone());
+        assert_eq!(steps, 24);
+
+        for r in robots.iter_mut() {
+            r.simulate(steps, (S_HEIGHT, S_WIDTH));
+        }
+        println!();
+        print_robots::<S_HEIGHT, S_WIDTH>(&robots);
+    }
 
     mod regression {
         use super::*;
@@ -171,6 +191,13 @@ p=9,5 v=-3,-3";
 
             assert_eq!(part1(&output), ANSWERS.0);
             assert_eq!(part2(&output), ANSWERS.1);
+
+            let mut robots = output;
+            for r in robots.iter_mut() {
+                r.simulate(ANSWERS.1, (HEIGHT, WIDTH));
+            }
+            println!();
+            print_robots::<HEIGHT, WIDTH>(&robots);
         }
     }
 }
