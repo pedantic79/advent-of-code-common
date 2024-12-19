3a614a67931eff7029e088b1588b65fd4c8fda80
Dan Nguyen <pedantic@gmail.com>
2024-12-19 16:32:21 -0600

    Day19: Manual search

--- src/day19.rs
+++ src/day19.rs
@@ -1,44 +1,70 @@
-use aho_corasick::AhoCorasick;
+use ahash::{HashMap, HashSet, HashSetExt};
 use aoc_runner_derive::{aoc, aoc_generator};
-use pathfinding::prelude::{count_paths, dfs};
 
 #[derive(Debug)]
 pub struct Input {
-    towels: AhoCorasick,
+    towels: Vec<HashSet<String>>,
     designs: Vec<String>,
 }
 
 impl Input {
-    fn solve1<'a>(&'a self, design: &'a str) -> Option<Vec<&'a str>> {
-        dfs(
-            design,
-            |&s| {
-                self.towels
-                    .find_overlapping_iter(s)
-                    .filter_map(|x| Some(&s[x.end()..]).filter(|_| x.start() == 0))
-            },
-            |&s| s.is_empty(),
-        )
+    fn solve1<'a>(&self, start: &'a str, memo: &mut HashMap<&'a str, bool>) -> bool {
+        if memo.contains_key(start) {
+            return memo[start];
+        }
+
+        for (i, pat) in self.towels.iter().enumerate().skip(1).take(start.len()) {
+            let (prefix, postfix) = start.split_at(i);
+
+            if pat.contains(prefix) && self.solve1(postfix, memo) {
+                memo.insert(start, true);
+                return true;
+            }
+        }
+
+        memo.insert(start, false);
+        false
     }
 
-    fn solve2(&self, design: &str) -> usize {
-        count_paths(
-            design,
-            |&s| {
-                self.towels
-                    .find_overlapping_iter(s)
-                    .filter_map(|x| Some(&s[x.end()..]).filter(|_| x.start() == 0))
-            },
-            |&s| s.is_empty(),
-        )
+    fn solve2<'a>(&self, start: &'a str, memo: &mut HashMap<&'a str, usize>) -> usize {
+        if memo.contains_key(start) {
+            return memo[start];
+        }
+
+        let mut combs = 0;
+        for (i, pat) in self.towels.iter().enumerate().skip(1).take(start.len()) {
+            let (prefix, postfix) = start.split_at(i);
+
+            if pat.contains(prefix) {
+                combs += self.solve2(postfix, memo);
+            }
+        }
+
+        memo.insert(start, combs);
+        combs
     }
 }
 
 #[aoc_generator(day19)]
 pub fn generator(input: &str) -> Input {
     let (a, b) = input.split_once("\n\n").unwrap();
+    let mut towels = Vec::new();
 
-    let towels = AhoCorasick::new(a.split(", ")).unwrap();
+    for s in a.split(", ") {
+        let s = s.to_string();
+        let len = s.len();
+        let mut towel_len = towels.len();
+
+        // make sure we have enough two
+        while len + 1 > towel_len {
+            towels.push(HashSet::new());
+            towel_len += 1;
+        }
+        assert!(towels.len() > s.len());
+
+        towels[len].insert(s);
+    }
+
     let designs = b.lines().map(|s| s.to_string()).collect();
 
     Input { towels, designs }
@@ -46,16 +72,24 @@ pub fn generator(input: &str) -> Input {
 
 #[aoc(day19, part1)]
 pub fn part1(inputs: &Input) -> usize {
+    let mut cache = HashMap::default();
+    cache.insert("", true);
     inputs
         .designs
         .iter()
-        .filter(|s: &&String| inputs.solve1(s).is_some())
+        .filter(|t| inputs.solve1(t, &mut cache))
         .count()
 }
 
 #[aoc(day19, part2)]
 pub fn part2(inputs: &Input) -> usize {
-    inputs.designs.iter().map(|s| inputs.solve2(s)).sum()
+    let mut cache = HashMap::default();
+    cache.insert("", 1);
+    inputs
+        .designs
+        .iter()
+        .map(|t| inputs.solve2(t, &mut cache))
+        .sum()
 }
 
 #[cfg(test)]
