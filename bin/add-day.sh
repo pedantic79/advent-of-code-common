7476889303d31d8353f4b124c5d8b3d447437cb6
Dan Nguyen <pedantic@gmail.com>
2025-02-01 09:53:50 -0600

    Support per-project year

--- bin/add-day.sh
+++ bin/add-day.sh
@@ -10,5 +10,12 @@ fi
 cp "src/template.rs" "src/day$NUM.rs"
 gsed -i "s/dayN/day$1/" "src/day$NUM.rs"
 gsed -i "/Insert before/i pub mod day$NUM;" "src/lib.rs"
-gsed -i "/Insert before/i - \[Day $1:\]\(https://adventofcode.com/2024/day/$1\)\n  - \[solution\]\(src/day$NUM.rs\)" README.md
+
+if [ -e .year ]; then
+    year=$(cat .year | tr -d '\n')
+else
+    year=$(date +%Y | tr -d '\n')
+fi
+
+gsed -i "/Insert before/i - \[Day $1:\]\(https://adventofcode.com/$year/day/$1\)\n  - \[solution\]\(src/day$NUM.rs\)" README.md
 cargo fmt
